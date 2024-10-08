ec76576459ff15d971b1d5f50ddcea7236019980
MinjuKang <minjukang727@gmail.com>
2024-10-09 00:41:11 +0900

    [Level 2] 8.QueryDSL - solution(3) Tuple 객체 사용
    [요구 사항]
    - JPQL로 작성된 `findByIdWithUser` 를 QueryDSL로 변경합니다.
    - 7번과 마찬가지로 N+1 문제가 발생하지 않도록 유의해 주세요!
    
    ** POINT! **
    - Level2. 8.QueryDSL - solution(1)에서 불필요한 필드까지 로드해 오는 부분을 수정
    - 모의면접 준비하를 하며 QueryDSL로 동적 쿼리 작성하는 방법에 있던 Tuple 객체를 사용해 보고 싶었다.
    
    [ 변경 사항 ]
    - TodoQueryRepositoryImpl.java
      - select()에서 TodoResponse 객체를 생성하는 데 필요한 필드만 지정
        - todo
        - user.id
        - user.email
        - user.nickname
      - fetchjoin()은 User Entity 전체를 로드해 오기 때문에 필요한 필드만 가져오고 싶어서 그냥 join() 사용
      - TodoService에서 Q객체를 사용하는 것이 나을까? Repository에서 TodoResponse로 반환하는게 나을지 고민하다 그냥 Repository에서 TodoResponse로 반환하였습니다.
    
      [ 날아간 쿼리 ]
     Hibernate:
         /* select
             todo,
             user.id,
             user.email,
             user.nickname
         from
             Todo todo
         inner join
             todo.user as user
         where
             todo.id = ?1 */ select
                 t1_0.id,
                 t1_0.contents,
                 t1_0.created_at,
                 t1_0.modified_at,
                 t1_0.title,
                 t1_0.user_id,
                 t1_0.weather,
                 u1_0.email,
                 u1_0.nickname
             from
                 todos t1_0
             join
                 users u1_0
                     on u1_0.id=t1_0.user_id
             where
                 t1_0.id=?
             limit
                 ?
    
     [참고 : join과 fetchjoin 차이점]
     - Join
       - 조인된 엔티티는 필요할 때, 추가 쿼리리 로드(지연로딩)
       - 기본적으로 N+1문제가 발생할 수 있습니다.
       - Lazy Loading 설정 적용
     - Fetch Join
       - 즉시 로딩 : 연관된 엔티티가 즉시 함께 로딩됩니다.
       - 추가 쿼리가 발생하지 않습니다.
       - Lazy Loading 설정이 무시됩니다.
       - 성능 상 이점이 있을 수 있지만 불필요하게 많은 데이터를 로드하면 성능 저하가 발생할 수 있습니다.
    
     (todoId로 todo 단건 조회의 경우,
     Todo Entity와 User Entity가 @ManyToOne 관계이고
     select 절에서 연관엔티티의 필드가 지정되어 있으므로 즉시 로드처럼 동작하여 N+1문제가 발생하지 않을 것이라 판단하고 코드를 작성하였습니다.
     그런데 개인적으로는 Tuple 객체를 사용한 방법보다는 Projections.constructor()를 사용한 방법이 TodoResponse 객체로 반환하기 더 좋았던 것 같습니다.)

--- src/main/java/org/example/expert/domain/todo/repository/TodoQueryRepositoryImpl.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoQueryRepositoryImpl.java
@@ -1,12 +1,11 @@
 package org.example.expert.domain.todo.repository;
 
-import com.querydsl.core.types.Projections;
+import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
-import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.user.dto.response.UserResponse;
 import org.springframework.stereotype.Repository;
 
@@ -22,26 +21,26 @@ public class TodoQueryRepositoryImpl implements TodoQueryRepository {
 
     @Override
     public TodoResponse findByIdWithUser(Long todoId) {
-        return jpaQueryFactory
-                .select(Projections.constructor(
-                        TodoResponse.class,
-                        todo.id,
-                        todo.title,
-                        todo.contents,
-                        todo.weather,
-                        Projections.constructor(
-                                UserResponse.class,
-                                todo.user.id,
-                                user.email,
-                                user.nickname
-                        ),
-                        todo.createdAt,
-                        todo.modifiedAt
-                ))
+        Tuple tuple = jpaQueryFactory
+                .select(todo,
+                        user.id,
+                        user.email,
+                        user.nickname
+                )
                 .from(todo)
                 .join(todo.user, user)
                 .where(todoIdEq(todoId))
                 .fetchFirst();
+
+        return new TodoResponse(
+                tuple.get(todo).getId(),
+                tuple.get(todo).getTitle(),
+                tuple.get(todo).getContents(),
+                tuple.get(todo).getWeather(),
+                new UserResponse(tuple.get(todo).getUser().getId(), tuple.get(user.email), tuple.get(user.nickname)),
+                tuple.get(todo).getCreatedAt(),
+                tuple.get(todo).getModifiedAt()
+        );
     }
 
     private BooleanExpression todoIdEq(Long todoId) {
